32eb05cea3cf8a0605b5ce604ba0233eeb9df681
SproutJ <SproutJ@gmail.com>
2021-10-01 04:58:06 +0900

    0915 커밋

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
 import React from 'react';
 
-function Food({fav}) {
-  return <h1>I like {fav}</h1>;
+function Food({name, picture}) {
+  return (
+    <div>
+    <h2>I like {name}</h2>
+    <img src={picture} />
+    </div>
+  );
 }
+
+const foodLike = [
+  {
+    name: 'strawberry',
+    image: 'https://t1.daumcdn.net/cfile/blog/99C56E4E5C495B0803',
+  },
+  {
+    name: 'juice',
+    image: 'https://lh3.googleusercontent.com/proxy/Cy3PBVWwTh1KVsNfTo9FFNCl33k5NsleiCKNvhBT0VfxsbKHO_-4eH6G8_Q0v2TaaDWG_JEKX-84xEZAmGRxGBhxi-i89B4t-cxcp6he-vP8fj1T',
+  },
+  {
+    name: 'tteokbokki',
+    image: 'https://cdn.kihoilbo.co.kr/news/photo/202008/880134_302005_3430.png',
+  }
+];
+
+const renderFood = dish => <Food name={dish.name} picture={dish.image} />;
+
+// function renderFood(dish){
+//   return <Food name={dish.name} picture={dish.image} />;
+// }
 
 function App() {
   return (
     <div>
       <h1>Hello React!!</h1>
-      <Food fav="kimchi" />
-      <Food fav="samgiosal" />
-      <Food fav="chukumi" />
-      </div>
+      {foodLike.map(renderFood)}
+    </div>
   );
 }
 
